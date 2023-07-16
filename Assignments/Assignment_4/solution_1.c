bf7556684c0885f3dfe7b110c152f3d2aedece5a
Shreyas Deshpande <shreyasdeshpande12@gmail.com>
2023-07-16 23:02:59 +0530

    Solution 1 done

---
+++ Assignments/Assignment_4/solution_1.c
@@ -0,0 +1,20 @@
+/*
+*Write a program which is used to demonstrate the concept of I/O redirection
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[]){
+
+    int result = 0, iNo1 = 0, iNo2 =0;
+
+    scanf("%d",&iNo1);
+    scanf("%d",&iNo2);
+
+    result = iNo1 + iNo2;
+
+    printf("Result = %d\n", result);
+
+    return 0;
+}
