126b6201610767509354554b9adbbc971cd9e71d
Shreyas Deshpande <shreyasdeshpande12@gmail.com>
2023-07-31 22:31:54 +0530

    Removed temperary files

--- Assignments/Assignment_8/Process_2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-
-int main(){
-    
-    printf("Child_Process-2:\tPID:%d\tPPID%d\n",getpid(),getppid());
-
-    return 0;
-}
--- Assignments/Assignment_8/Process_3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-
-int main(){
-    
-    printf("Child_Process-3:\tPID:%d\tPPID%d\n",getpid(),getppid());
-
-    return 0;
-}
