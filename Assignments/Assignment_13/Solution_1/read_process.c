ec9991436adb942f14b4472046a15dcbee5f66e8
Shreyas Deshpande <shreyasdeshpande12@gmail.com>
2023-09-02 22:28:03 +0530

    Solution_1 Done

---
+++ Assignments/Assignment_13/Solution_1/read_process.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main(int argc, char* argv[]){
+    int fd =0;
+    char* path_fifo = "/tmp/mypipe";
+    char Buffer[100];
+
+    printf("Client is running for reading data from named pipe\n");
+
+    fd = open(path_fifo,O_RDONLY);
+    if(fd==-1){
+        printf("Unable to open named pipe for reading\n");
+        return -1;
+    }
+
+    read(fd,Buffer,20);
+
+    printf("Daata read from named pipe is:\n%s\n",Buffer);
+
+    return 0;
+}
