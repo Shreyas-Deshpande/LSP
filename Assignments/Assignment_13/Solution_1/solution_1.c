18fe43f1d34d052a0ef91b3886e1e032508142c9
Shreyas Deshpande <shreyasdeshpande12@gmail.com>
2023-09-02 22:38:20 +0530

    Reformmatted Code

--- Assignments/Assignment_13/Solution_1/solution_1.c
+++ Assignments/Assignment_13/Solution_1/solution_1.c
@@ -3,39 +3,40 @@
  * and writer and those processes should communicate with
  * each other using FIFO
  */
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int fd = 0, ret = 0;
-	char* fifo_path = "/tmp/mypipe";
+	char *fifo_path = "/tmp/mypipe";
 
 	fd = mkfifo(fifo_path, 0666);
 
-	if(fd == -1)
+	if (fd == -1)
 	{
 		printf("Unable to create named pipe\n");
 		return -1;
 	}
 
-	//create new process using fork and execl
+	// create new process using fork and execl
 	ret = fork();
-	if(ret == 0){
-		//from child process read the pipe;
-		execl("./read_fifo","NULL",NULL);
+	if (ret == 0)
+	{
+		// from child process read the pipe;
+		execl("./read_fifo", "NULL", NULL);
 	}
-	else{
+	else
+	{
 		printf("Server is runnig and writing data in named pipe\n");
-		fd = open(fifo_path,O_WRONLY);
-		write(fd,"This is shared data.",20);
+		fd = open(fifo_path, O_WRONLY);
+		write(fd, "This is shared data.", 20);
 		close(fd);
 		printf("Data wriiten in named pipe succesfully\n");
 		unlink(fifo_path);
 	}
 	return 0;
 }
-
