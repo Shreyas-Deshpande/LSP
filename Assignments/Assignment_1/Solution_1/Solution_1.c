bffa7440025a5f83c2daea875ad1dc68a9da5e16
Shreyas Deshpande <shreyasdeshpande12@gmail.com>
2023-09-14 23:26:33 +0530

    Makefile added

---
+++ Assignments/Assignment_1/Solution_1/Solution_1.c
@@ -0,0 +1,34 @@
+/*
+*Problem Statement:
+*   Write a program which accept file name from user and open that file.
+*/
+
+
+#include<stdio.h>// For printf
+#include<fcntl.h> //for file mode definations(MACROS)
+#include<unistd.h> //for close system call
+
+int main(int argc, char* argv[]){\
+    int fd =0;
+
+    if(argc!=2){
+        printf("Please enter the file name as command line argument\n");
+        return -1;
+    }
+
+
+    fd = open(argv[1],O_RDONLY);
+
+    if(fd == -1){
+        printf("Can't Open File\n");
+        return -1;
+    }
+    else{
+        printf("File gets opened succesfully\n");
+        printf("File Descriptor: %i\n",fd);
+    }
+
+    close(fd);
+    return 0;
+    
+}
